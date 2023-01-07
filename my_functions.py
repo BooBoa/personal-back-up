7e2447f3a4a2f601e04c78bd0ae914bc6fbd3172
Dennis Rostron <121449406+BooBoa@users.noreply.github.com>
2023-01-07 17:58:13 +0000

    Update my_functions.py

--- my_functions.py
+++ my_functions.py
@@ -208,7 +208,7 @@ def test_step(model: torch.nn.Module,
     test_loss = test_loss / len(dataloader)
     test_acc = test_acc / len(dataloader)
     
-    return test_loss, test_acct
+    return test_loss, test_acc
  
 def train(model: torch.nn.Module, 
           train_dataloader: torch.utils.data.DataLoader, 

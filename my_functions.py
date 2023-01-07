4056ccced7b2a667aa5623a9f429fe694298476b
Dennis Rostron <121449406+BooBoa@users.noreply.github.com>
2023-01-07 19:13:34 +0000

    Update my_functions.py

--- my_functions.py
+++ my_functions.py
@@ -227,6 +227,7 @@ def train(model: torch.nn.Module,
     
     # 3. Loop through training and testing steps for a number of epochs
     for epoch in tqdm(range(epochs)):
+        print(f"\n°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸\nEpoch {epoch+1}")
         train_loss, train_acc = train_step(model=model,
                                            dataloader=train_dataloader,
                                            loss_fn=loss_fn,

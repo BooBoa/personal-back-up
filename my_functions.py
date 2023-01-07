d4b0a95bd2701700a72fc30267842aedc2a89899
Dennis Rostron <121449406+BooBoa@users.noreply.github.com>
2023-01-07 15:58:29 +0000

    Update my_functions.py

--- my_functions.py
+++ my_functions.py
@@ -20,7 +20,7 @@ import zipfile
 from pathlib import Path
 from typing import List
 import requests
-from Pil import Image
+from PIL import Image
 device = "cuda" if torch.cuda.is_available() else "cpu"
 #def import_matrix():
 #  #try:

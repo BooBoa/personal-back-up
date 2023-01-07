7266035e223b4248c51d4e99f9eb280c7b32cea9
Dennis Rostron <121449406+BooBoa@users.noreply.github.com>
2023-01-07 20:45:37 +0000

    Update my_functions.py

--- my_functions.py
+++ my_functions.py
@@ -181,11 +181,12 @@ def train_step(model: torch.nn.Module,
 def test_step(model: torch.nn.Module, 
               dataloader: torch.utils.data.DataLoader, 
               loss_fn: torch.nn.Module):
-    
     # Put model in eval mode
     model.eval() 
+    
     # Setup test loss and test accuracy values
     test_loss, test_acc = 0, 0
+    
     # Turn on inference context manager
     with torch.inference_mode():
         # Loop through DataLoader batches
@@ -194,20 +195,20 @@ def test_step(model: torch.nn.Module,
             X, y = X.to(device), y.to(device)
     
             # 1. Forward pass
-            test_logits = model(X)
+            test_pred_logits = model(X)
 
             # 2. Calculate and accumulate loss
-            t_loss = loss_fn(test_logits, y)
-            test_loss += t_loss.item()
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
             
             # Calculate and accumulate accuracy
-            test_pred_labels = torch.argmax(torch.softmax(test_logits, dim=1), dim=1)
+            test_pred_labels = test_pred_logits.argmax(dim=1)
             test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
             
-        # Adjust metrics to get average loss and accuracy per batch 
-        test_loss = test_loss / len(dataloader)
-        test_acc = test_acc / len(dataloader)
-        return test_loss, test_acc
+    # Adjust metrics to get average loss and accuracy per batch 
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
  
 # 1. Take in various parameters required for training and test steps
 # 1. Take in various parameters required for training and test steps

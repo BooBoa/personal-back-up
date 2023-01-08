23dc9858e02b84193e7e85c98bd0e9221cb46d95
Dennis Rostron <121449406+BooBoa@users.noreply.github.com>
2023-01-08 00:23:49 +0000

    Update my_functions.py

--- my_functions.py
+++ my_functions.py
@@ -169,11 +169,11 @@ def train_step(model: torch.nn.Module,
 
         # 5. Optimizer step
         optimizer.step()
-    sheduler.step() 
+    
         # Calculate and accumulate accuracy metric across all batches
         y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
         train_acc += (y_pred_class == y).sum().item()/len(y_pred)
-
+    scheduler.step()
     # Adjust metrics to get average loss and accuracy per batch 
     train_loss = train_loss / len(dataloader)
     train_acc = train_acc / len(dataloader)
